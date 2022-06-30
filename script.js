287d10cc30b2e3ef26867de6ad197331eb81a2cc
Johan <jpettersen50@gmail.com>
2022-06-30 02:24:57 +0200

    DEL script.js original CHANGE script src + script2

--- script.js
+++ script.js
@@ -1,127 +1,168 @@
-/* <---- calculator ----->
-
-Add
-
-Subtract
-
-Multiply
-
-Divide
-
-*/
-
 const numButtons = document.querySelectorAll(".numButton");
 const displayNumbers = document.querySelector(".display");
 const operatorButton = document.querySelectorAll(".operatorButton");
 
-// ---- why doesn't it work? -----
-console.log('yep');
 
 
+
+//buttons set operator when pressed. Operator is later used when using operate()
+//operate() gets operator to determine what function to call, add(), subtract() etc.
+// number buttons (0-9) calls addToDisplay() where secondaryValue and textContent of displayNumbers is set
+
+
+
+let displayValue = "0";
+let operator;
+let secondaryValue;
+displayNumbers.textContent = displayValue;
+
+
+function add() {
+
+    console.log("displayValue: " + displayValue + " secondaryValue: " + secondaryValue)
+    displayValue = Number(displayValue);
+    displayValue += Number(secondaryValue);
+    
+    secondaryValue = "";
+    displayNumbers.textContent = displayValue;
+    console.log("displayValue: " + displayValue + " secondaryValue: " + secondaryValue)
+}
+
+
+function subtract() {
+console.log(displayValue)
+
+    if(displayValue === 0 || displayValue === '0') {
+        displayValue = secondaryValue;
+        secondaryValue = "";
+        displayNumbers.textContent = displayValue;
+    } else {
+        console.log("1. displayValue: " + displayValue + "secondaryValue: " + secondaryValue)
+    displayValue = Number(displayValue);
+    displayValue -= Number(secondaryValue);
+    console.log("2. displayValue: " + displayValue + "secondaryValue: " + secondaryValue)
+    secondaryValue = "";
+    displayNumbers.textContent = displayValue;
+    console.log("3. displayValue: " + displayValue + "secondaryValue: " + secondaryValue)
+}
+}
+
+
+function multiply() {
+
+    if(displayValue === 0 || displayValue === '0') {
+        displayValue = secondaryValue;
+        secondaryValue = "";
+        displayNumbers.textContent = displayValue;
+    } else {
+    displayValue = Number(displayValue);
+    displayValue *= Number(secondaryValue);
+    secondaryValue = "";
+    displayNumbers.textContent = displayValue;
+}
+}
+
+
+function divide() {
+console.log("displayValue: " + displayValue + " secondary: " + secondaryValue)
+
+    if(displayValue === 0 || displayValue === '0') {
+        displayValue = secondaryValue;
+        secondaryValue = "";
+        displayNumbers.textContent = displayValue;
+    } else {
+    displayValue = Number(displayValue);
+    displayValue /= Number(secondaryValue);
+    secondaryValue = "";
+    displayNumbers.textContent = displayValue;
+}
+}
+
+
+function operate() {
+    
+    if(operator === '+') {
+        add();
+    } 
+    else if(operator === '-') {
+        subtract();
+    }
+    else if(operator === '*') {
+        multiply();
+    }
+    else if(operator === '/') {
+        if(secondaryValue === 0 || secondaryValue === '0') {
+            alert('no');
+        }else {
+        divide();
+    }}
+
+
+}
 
 function addToDisplay(num) {
-    if (displayNumbers.textContent === '0') {
-        displayNumbers.textContent = "";
-        displayNumbers.textContent += num;
-    } else {
-        displayNumbers.textContent += num;
+    if(secondaryValue === undefined) {
+        secondaryValue = num;
+        displayNumbers.textContent = secondaryValue;
+    }else {
+        secondaryValue += num;
+        displayNumbers.textContent = secondaryValue;
     }
 
 }
 
 function clearAll() {
     displayNumbers.textContent = 0;
-    num1 = "";
+    displayValue = "0";
+    secondaryValue = "";
     operator = "";
 
 }
 
-
 numButtons.forEach((btn) => {
-
 
     if (btn.id == "num1Button") {
         btn.addEventListener('click', () => {
-            addToDisplay(1);
+            addToDisplay('1');
         });
     } else if (btn.id == "num2Button") {
         btn.addEventListener('click', () => {
-            addToDisplay(2);
+            addToDisplay("2");
         });
     } else if (btn.id == "num3Button") {
         btn.addEventListener('click', () => {
-            addToDisplay(3);
+            addToDisplay("3");
         });
     } else if (btn.id == "num4Button") {
         btn.addEventListener('click', () => {
-            addToDisplay(4);
+            addToDisplay("4");
         });
     } else if (btn.id == "num5Button") {
         btn.addEventListener('click', () => {
-            addToDisplay(5);
+            addToDisplay("5");
         });
     } else if (btn.id == "num6Button") {
         btn.addEventListener('click', () => {
-            addToDisplay(6);
+            addToDisplay("6");
         });
     } else if (btn.id == "num7Button") {
         btn.addEventListener('click', () => {
-            addToDisplay(7);
+            addToDisplay("7");
         });
     } else if (btn.id == "num8Button") {
         btn.addEventListener('click', () => {
-            addToDisplay(8);
+            addToDisplay("8");
         });
     } else if (btn.id == "num9Button") {
         btn.addEventListener('click', () => {
-            addToDisplay(9);
+            addToDisplay("9");
         });
     } else if (btn.id == "num0Button") {
         btn.addEventListener('click', () => {
-            addToDisplay(0);
+            addToDisplay("0");
         });
     }
-
 });
-
-let sum = 0;
-let num1 = Number(displayNumbers.textContent);
-let operator;
-
-
-/** initialVar
- * +=
- * next num
- * 
- * 
- * 5 + 5 = 10 + 5 = 15 + 5 = 20 + 5
- * 
- * num1 += nextnum
- * 
- * 
- * click num > show num > click + > show num += new num
- * 
- *  sum += num1
- *  sum -= num1
-    sum *= num1
-    sum /= num1
-
- */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 operatorButton.forEach((btn) => {
 
@@ -130,8 +171,8 @@ operatorButton.forEach((btn) => {
 
         btn.addEventListener('click', () => {
 
-            console.log(sum, num1);
-            add(sum, num1);
+            operator = '+'
+            add();
 
 
         });
@@ -140,24 +181,16 @@ operatorButton.forEach((btn) => {
     if (btn.id == "subtractButton") {
 
         btn.addEventListener('click', () => {
-            if (operator != undefined) {
-                num1 = Number(num1);
-                num1 -= Number(displayNumbers.textContent);
-                displayNumbers.textContent = "";
-            }else {
-            operator = '-';
-            num1 = displayNumbers.textContent;
-            displayNumbers.textContent = "";
-        }
+            operator = '-'
+            subtract();
         });
     }
 
     if (btn.id == "multiplyButton") {
 
         btn.addEventListener('click', () => {
-            operator = '*';
-            num1 = displayNumbers.textContent;
-            displayNumbers.textContent = "";
+            operator = '*'
+            multiply();
 
         });
     }
@@ -165,18 +198,15 @@ operatorButton.forEach((btn) => {
     if (btn.id == "divideButton") {
 
         btn.addEventListener('click', () => {
-            operator = '/';
-            num1 = displayNumbers.textContent;
-            displayNumbers.textContent = "";
+            operator = '/'
+            divide();
 
         });
     }
 
     if (btn.id == "equalsButton") {
         btn.addEventListener('click', () => {
-            operate(operator, Number(num1), Number(displayNumbers.textContent));
-
-
+            operate(operator);
         });
     }
 
@@ -184,56 +214,3 @@ operatorButton.forEach((btn) => {
         btn.addEventListener('click', clearAll);
     }
 });
-
-
-
-
-
-
-
-
-
-function add(sum, num1) {
-    
-    sum = Number(sum);
-    sum += num1;
-    displayNumbers.textContent = sum;
-}
-
-function subtract(num1, num2) {
-    
-    num1 -= num2;
-    displayNumbers.textContent = num1;
-}
-
-function multiply(num1, num2) {
-    
-    num1 *= num2;
-    displayNumbers.textContent = num1;
-}
-
-function divide(num1, num2) {
-    
-    num1 /= num2;
-    displayNumbers.textContent = num1;
-}
-
-function operate(operator, num1, num2) {
-    console.log(operator, num1, num2);
-
-
-
-    if (operator === '+') {
-        add(num1, num2);
-    } else if (operator === '-') {
-        subtract(num1, num2);
-    } else if (operator === "*") {
-        multiply(num1, num2);
-    } else if (operator === "/") {
-        divide(num1, num2);
-    }
-
-
-
-
-}
